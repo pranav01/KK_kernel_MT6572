04f601721653d2217c918aa2c633f4bb63d604ae
hyperion70 <bmaximov71@gmail.com>
2015-03-04 20:03:31 +0300

    Fix leds drv. Accelerometer -> KXTJ2_1009-new.

--- mediatek/custom/golf/kernel/leds/mt65xx/cust_leds.c
+++ mediatek/custom/golf/kernel/leds/mt65xx/cust_leds.c
@@ -1,6 +1,5 @@
 #include <cust_leds.h>
 #include <mach/mt_pwm.h>
-#include "cust_leds_def.h"
 
 #include <linux/kernel.h>
 #include <mach/pmic_mt6329_hw_bank1.h> 
@@ -10,8 +9,8 @@
 #include <mach/upmu_common_sw.h>
 #include <mach/upmu_hw.h>
 
-extern int mtkfb_set_backlight_level(unsigned int level);
-extern int mtkfb_set_backlight_pwm(int div);
+//extern int mtkfb_set_backlight_level(unsigned int level);
+//extern int mtkfb_set_backlight_pwm(int div);
 extern int disp_bls_set_backlight(unsigned int level);
 /*
 #define ERROR_BL_LEVEL 0xFFFFFFFF
@@ -25,28 +24,28 @@ unsigned int brightness_mapping(unsigned int level)
 {
     unsigned int mapped_level;
     
-	    mapped_level = level/4; //1-wire control in S5 phone only has 16 step 
+    mapped_level = level/4;
        
 	return mapped_level;
 }
 
 unsigned int Cust_SetBacklight(int level, int div)
 {
-    mtkfb_set_backlight_pwm(div);
-    mtkfb_set_backlight_level(brightness_mapping(level));
+    //mtkfb_set_backlight_pwm(div);
+    //mtkfb_set_backlight_level(brightness_mapping(level));
     disp_bls_set_backlight(brightness_mapping(level));
     return 0;
 }
 
 
 static struct cust_mt65xx_led cust_led_list[MT65XX_LED_TYPE_TOTAL] = {
-	{"red",               MT65XX_LED_MODE_NONE, -1,{0}},
-	{"green",             MT65XX_LED_MODE_NONE, -1,{0}},
+	{"red",               MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK1,{0}},
+	{"green",             MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK0,{0}},
 	{"blue",              MT65XX_LED_MODE_NONE, -1,{0}},
 	{"jogball-backlight", MT65XX_LED_MODE_NONE, -1,{0}},
 	{"keyboard-backlight",MT65XX_LED_MODE_NONE, -1,{0}},
-	{"button-backlight",  MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK0,{0}},
-	{"lcd-backlight",     MT65XX_LED_MODE_PWM, PWM1,{0}},
+	{"button-backlight",  MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK2,{0}},
+	{"lcd-backlight",     MT65XX_LED_MODE_PWM, PWM2,{0}},
 };
 
 struct cust_mt65xx_led *get_cust_led_list(void)
